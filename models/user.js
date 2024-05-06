4e305f5880cde1936028eeb197ac9d4be9dfa105
ayushpasi <pasiayush999@gmail.com>
2024-05-06 23:30:16 +0530

    Deleting Cart Items

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongodb = require("mongodb");
+const { ValidationErrorItem } = require("sequelize");
 const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 class User {
@@ -56,6 +57,39 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
   static findById(userId) {
     const db = getDb();
     return db.collection("users").findOne({ _id: new ObjectId(userId) });
